aaea233c3d472cc1230fef5cdc060b1aaba88de5
uyt6u7r <67103688+uyt6u7r@users.noreply.github.com>
2022-05-20 01:05:02 +1000

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,14 @@ var config = {
         "I love you, Not only for what you are, But for what I am, When I am with you.",
         "I love you, Not only for what you have made of yourself, But for what you are making of me.",
         "I love you, For the part of me That you bring out;",
-        "I love you, For putting your hand Into my heaped-up heart, And passing over",
-        "All the foolish, weak things, That you can’t help",
-        "爱你永远。。。",
+        "I love you, For putting your hand Into my heaped-up heart, And passing over All the foolish, weak things, That you can’t help",
+        "Dimly seeing there, And for drawing out Into the light. All the beautiful belongings That no one else had looked Quite far enough to find.",
+        "I love you because you Are helping me to make Of the lumber of my life",
+        "Not a tavern, But a temple;",
+        "Out of the works, Of my every day;",
+        "Not a reproach, But a song.",
+        "Without a touch, Without a word, Without a sign.",
+        "You have done it By being yourself.",
     ],
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
